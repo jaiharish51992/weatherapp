2bdc9b8779b03690a3869e3c14487a5234d7f9a9
jaiharish51992 <jayashreeharish.ohm@gmail.com>
2025-02-01 12:53:48 +0530

    weather

---
+++ weather.js
@@ -0,0 +1,19 @@
+const apiKey = "e610cff0d774065e800b661bad72603f";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q= ";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city +  `&appid=${apiKey}`);
+    const data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp ) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+ }
+    searchBtn.addEventListener("click", () => {
+        const city = searchBox.value;
+        checkWeather(city);
+    });
+    
+    
